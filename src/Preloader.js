22bd3ce781e9a613c64c87ec81ecb458d6e25c1b
rntmdgus <rntmdgus@naver.com>
2014-06-05 20:33:29 +0900

    ssss

--- src/Preloader.js
+++ src/Preloader.js
@@ -26,7 +26,7 @@ BasicGame.Preloader.prototype = {
 		//	As this is just a Project Template I've not provided these assets, the lines below won't work as the files themselves will 404, they are just an example of use.
 		this.load.image('mainBackground', 'images/main_background.png');
 		this.load.image('logo', 'images/logo.png');
-		this.load.image('stageBackground', 'images/stage_background.png');
+		this.load.image('settingBackgroun', 'images/setting_background.png');
 
 		this.load.image('playButton', 'images/play_button.png');
 		this.load.image('settingsButton', 'images/settings_button.png');
@@ -41,6 +41,8 @@ BasicGame.Preloader.prototype = {
 		this.load.image('stage5Button', 'images/planet5.png');
 
 		this.load.audio('titleMusic', ['audio/main_menu.mp3']);
+		this.load.audio('stageMusic', ['audio/stage.mp3']);
+
 		//	+ lots of other required assets here
 
 	},
